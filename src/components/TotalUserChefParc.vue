5d52f03f51a53ea3583be033432ca7c0aa043bd0
steeve <kevinandrianarison25@gmail.com>
2025-01-22 17:27:14 +0100

    load

--- src/components/TotalUserChefParc.vue
+++ src/components/TotalUserChefParc.vue
@@ -3,7 +3,7 @@
     <div
       class="border w-60 gap-2 py-4 bg-white rounded-lg shadow-sm flex flex-col items-center justify-center"
     >
-      <div class="img h-7 w-7 rounded-3xl"></div>
+      <div class="img h-7 w-7 rounded-3xl ring-4 ring-yellow-500"></div>
       <p class="text-yellow-500">Chef de parcours</p>
       <p><b>Effectifs :</b> {{ nombre }}</p>
     </div>
